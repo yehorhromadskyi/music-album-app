705cdc77f3885e85955373ebad42f52a6dac216b
Yehor Hromadskyi <yhrom@softserveinc.com>
2018-09-01 18:23:51 +0300

    Albums activity progress bar

--- src/MusicAlbumApp.Core/ViewModels/AlbumsViewModel.cs
+++ src/MusicAlbumApp.Core/ViewModels/AlbumsViewModel.cs
@@ -11,11 +11,18 @@ namespace MusicAlbumApp.Core.ViewModels
         readonly IAlbumsApiService _albumsApiService;
 
         private List<Album> _albums;
+        private bool _isBusy;
 
         public List<Album> Albums
         {
             get => _albums;
             set => SetProperty(ref _albums, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
         }
 
         public AlbumsViewModel(IAlbumsApiService albumsApiService)
@@ -25,11 +32,15 @@ namespace MusicAlbumApp.Core.ViewModels
 
         public override async Task Initialize()
         {
+            IsBusy = true;
+
             var albums = await _albumsApiService.GetAlbumsAsync();
 
             Albums = albums;
 
             await base.Initialize();
+
+            IsBusy = false;
         }
     }
 }
